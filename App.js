5f423c4178399fc9bff427df706a6136f6f09e84
habadong <jiyong.ha@kakao.com>
2020-04-29 14:55:50 +0900

    기본 화면구성

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, TextInput, Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle='light-content'/>
-
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>RN-To do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"} />
+        </View>
       </View>
     )
   }
@@ -21,13 +19,26 @@ class App extends React.Component {
 
 
 const styles = StyleSheet.create({
-
   container : {
     flex : 1,
-    backgroundColor : '#fff',
+    backgroundColor : '#F23657',
     alignItems : 'center',
-    justifyContent : 'center',
+  
   },
+  title : {
+    color : 'white',
+    fontSize : 30,
+    marginTop : 50,
+    fontWeight : '200',
+    marginBottom : 30
+  },
+  card : {
+    backgroundColor : 'white',
+    flex : 1,
+    width : width - 25,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10
+  }
 });
 
 export default App;
